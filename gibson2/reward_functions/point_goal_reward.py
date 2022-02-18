cd782acfebf19418d19329e063222628e2bc32b9
Feng Gao <gaof.student@gmail.com>
2022-02-18 03:52:26 -0500

    add false_stop_reward

--- gibson2/reward_functions/point_goal_reward.py
+++ gibson2/reward_functions/point_goal_reward.py
@@ -13,6 +13,9 @@ class PointGoalReward(BaseRewardFunction):
         self.success_reward = self.config.get(
             'success_reward', 10.0
         )
+        self.false_stop_reward = self.config.get(
+            'false_stop_reward', 0.0
+        )
         self.dist_tol = self.config.get('dist_tol', 0.5)
         print('success_reward', self.success_reward)
 
@@ -25,11 +28,17 @@ class PointGoalReward(BaseRewardFunction):
         :param env: environment instance
         :return: reward
         """
-        success = l2_distance(
+        game_should_over = l2_distance(
             env.robots[0].get_position()[:2],
             task.target_pos[:2]) < self.dist_tol
-        success = success and (action is None)
-        reward = self.success_reward if success else 0.0
+        success = game_should_over and (action is None)
+        if not game_should_over and (action is None):
+            reward = self.false_stop_reward
+        elif success:
+            reward = self.success_reward
+        else:
+            reward = 0.0
+        # reward = self.success_reward if success else 0.0
         return reward
 
     def update_weights(self, new_weights):
