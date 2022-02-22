992f5be4a87f3960b5171f4a25f759d3b998363f
Feng Gao <gaof.student@gmail.com>
2022-02-22 07:34:49 -0500

    update

--- gibson2/termination_conditions/point_goal.py
+++ gibson2/termination_conditions/point_goal.py
@@ -11,6 +11,7 @@ class PointGoal(BaseTerminationCondition):
     def __init__(self, config):
         super(PointGoal, self).__init__(config)
         self.dist_tol = self.config.get('dist_tol', 0.5)
+        self.require_stop = self.config.get('REQUIRE_ACTIVE_STOP', False)
 
     def get_termination(self, task, env, action):
         """
@@ -24,7 +25,10 @@ class PointGoal(BaseTerminationCondition):
         task_done = l2_distance(
             env.robots[0].get_position()[:2],
             task.target_pos[:2]) < self.dist_tol
-        done = task_done #or (action is None) 
-        # done = (action is None)
-        success = task_done # and (action is None)
+        if self.require_stop:
+            done = action is None
+            success = task_done and (action is None)
+        else:
+            done = task_done
+            success = task_done
         return done, success
